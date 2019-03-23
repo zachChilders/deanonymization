9880469dc0db8c6aa7f1afbb70ecfe81e5082d08
Zach <zach.childers@ischool.berkeley.edu>
2019-03-23 11:55:07 -0700

    postgres connection works

--- ingress/app/connect.py
+++ ingress/app/connect.py
@@ -1,9 +1,7 @@
-# Remove this connection string
 import os
 import postgres
 
 connstring = os.environ["ingressconnectionstring"]
-
 client = postgres.Postgres(connstring)
-q = client.one("SELECT * FROM t1")
+q = client.one("SELECT * FROM account;")
 print(q)
