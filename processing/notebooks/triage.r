367e13559e87d164813c344ff4a1da9517a2435c
Zach Childers <zwc101@gmail.com>
2019-04-13 16:52:10 +0000

    Merged PR 12: Added Triage framework
    Sets up a framework for triaging.   Just add Kanonymity.  I also established a couple patterns here to use in the future notebooks.
    
    Much of this work is external (in the github library)
    - setTable allows us to write back into the metadata table
    - We have an Enum exposed - states - that controls our Finite State Machine

--- processing/notebooks/triage.r
+++ processing/notebooks/triage.r
@@ -1,3 +1,11 @@
+minimumKValue <- 4 # This needs to be tuned.
+
+# K-anonymity function to triage tables
+# TODO: write this
+Kanon <- function(table) {
+    5
+}
+
 #Bootstrap deanonymize library
 library('devtools')
 if (!require('deanonymizeR')) {
@@ -5,4 +13,25 @@ if (!require('deanonymizeR')) {
     library('deanonymizeR')
 }
 
+# Get metadata table
 index <- getTable('tables_index')
+
+# Select ready for triage
+scraped <- subset(index, state == states$SCRAPESUCCESS)
+
+# Select table
+triage <- scraped[complete.cases(scraped$table_name),][1,]
+tableRowToTriage <- triage$table_name
+table <- getTable(tableRowToTriage)
+
+# Run K-anon or something
+result <- Kanon(table) 
+if (result > minimumKValue) {
+  state <- states$TRIAGESUCCESS
+} else {
+  state <- states$TRIAGEFAIL
+}
+
+# Set 
+setTable(tableRowToTriage, state)
+
