b1e5ac6bda1c63950f4edbf9e3a26570f7c0aa26
Aaron Crouch <mehmac@mehmac.local>
2019-04-13 17:16:47 -1000

    commit message: updated k-anon, and commented out interaction

--- processing/notebooks/triage.r
+++ processing/notebooks/triage.r
@@ -3,6 +3,40 @@ minimumKValue <- 4 # This needs to be tuned.
 # K-anonymity function to triage tables
 # TODO: write this
 Kanon <- function(table) {
+    function [k, equivalence_classes] = kAnonymity_Analyze(data_table, quasi_identifiers)
+    % Let's first get the unique rows when the table is projected only on
+    % quasi identifiers. This will yield the equivalence classes as a data
+    % table (and will return it as a second return value, if the function
+    % call requested two return values)
+    equivalence_classes = unique(data_table(:,quasi_identifiers), 'rows');
+    % We will now augment this table with a new column, called "k" that
+    % counts for each equivalence class, the number of rows in the original
+    % table "data_table" that belong to that equivalence class.
+    equivalence_classes.k(:) = NaN;
+    for eqv_class_id = 1:size(equivalence_classes, 1)
+        % these are the values of the qids for the equivalence class
+        eqv_class = equivalence_classes{eqv_class_id,quasi_identifiers};
+        
+        table = ones;
+        i = 1;
+        for attribute = quasi_identifiers
+           col = table2array(data_table(:, attribute));
+           bool = col == eqv_class(i);
+           table = table .* bool;
+           i = i+1;
+        end
+            
+        equivalence_classes.k(eqv_class_id) = sum(table); 
+    end
+    % The k in k-Anonymity is the size of the smallest equivalence class.
+    % TODO: complete this code to assign the return value k
+    k = min(equivalence_classes.k); % TODO: fill this here
+end
+    
+    
+    
+    
+    
     5
 }
 
@@ -33,5 +67,5 @@ if (result > minimumKValue) {
 }
 
 # Set 
-setTable(tableRowToTriage, state)
+# setTable(tableRowToTriage, state)   // uncomment later, when zach says its ok
 
